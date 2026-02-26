43e8e162bad36ebac4ffe57d5a4fc48444232db7
protoplasmic.cfg <119766757+protoplasmiccfg@users.noreply.github.com>
2026-02-26 20:03:41 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const cacheName = 'ktpower-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  );
+});
